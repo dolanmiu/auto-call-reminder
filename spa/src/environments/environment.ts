454e6b11454cf247947d1c03d11f87ddc35793ca
Dolan Miu <dolan_miu@hotmail.com>
2022-02-26 12:02:58 +0000

    Update firebase config

--- spa/src/environments/environment.ts
+++ spa/src/environments/environment.ts
@@ -7,16 +7,7 @@ import { FIREBASE_SDK_CONFIG } from './firebase.conf';
 
 export const environment: Environment = {
   production: false,
-  firebase: {
-    projectId: 'phone-scheduler',
-    appId: '1:705171398397:web:abe32957269b45c0862894',
-    storageBucket: 'phone-scheduler.appspot.com',
-    locationId: 'europe-west',
-    apiKey: 'AIzaSyCMslVeC62ecvtzsC-LLXEngfAv7SKYp4k',
-    authDomain: 'phone-scheduler.firebaseapp.com',
-    messagingSenderId: '705171398397',
-    measurementId: 'G-SYNJCZTPWN',
-  },
+  firebase: FIREBASE_SDK_CONFIG,
   brandName: 'Phone Scheduler',
 };
 
--- spa/src/environments/firebase.conf.ts
+++ spa/src/environments/firebase.conf.ts
@@ -1,9 +1,10 @@
 export const FIREBASE_SDK_CONFIG = {
+  projectId: "phone-scheduler",
+  appId: "1:705171398397:web:abe32957269b45c0862894",
+  storageBucket: "phone-scheduler.appspot.com",
+  locationId: "europe-west",
   apiKey: "AIzaSyCMslVeC62ecvtzsC-LLXEngfAv7SKYp4k",
   authDomain: "phone-scheduler.firebaseapp.com",
-  projectId: "phone-scheduler",
-  storageBucket: "phone-scheduler.appspot.com",
   messagingSenderId: "705171398397",
-  appId: "1:705171398397:web:abe32957269b45c0862894",
   measurementId: "G-SYNJCZTPWN",
 };
