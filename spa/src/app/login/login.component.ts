4fa4b3e00c122421a27079ceba9857183f701623
Dolan Miu <dolan_miu@hotmail.com>
2022-02-27 12:42:59 +0000

    Add user/pass authentication

--- spa/src/app/login/login.component.ts
+++ spa/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../shared/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,7 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-  constructor() {}
+  public readonly loginForm: FormGroup;
+
+  constructor(fb: FormBuilder, private readonly authService: AuthService) {
+    this.loginForm = fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
 
   ngOnInit(): void {}
+
+  public async login(): Promise<void> {
+    if (!this.loginForm.valid) {
+      return;
+    }
+
+    console.log('logging in', this.loginForm.value);
+    await this.authService.emailSignin(
+      this.loginForm.get('username')?.value,
+      this.loginForm.get('password')?.value
+    );
+  }
 }
