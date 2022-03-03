8cf1ded2c4682933ba98e28a40f8d305d13c87f2
Dolan Miu <dolan_miu@hotmail.com>
2022-03-03 03:10:49 +0000

    Add time out to call

--- functions/src/util/make-call.ts
+++ functions/src/util/make-call.ts
@@ -34,6 +34,7 @@ export const makeCall = async (
     to: toNumber,
     from: "+447488880401",
     record: true,
+    timeout: 7,
     statusCallback: `https://us-east4-phone-scheduler.cloudfunctions.net/writeCall?userUid=${userUid}&callConfigUid=${callConfigUid}`,
     statusCallbackEvent: ["ringing", "answered", "completed"],
     statusCallbackMethod: "POST",
